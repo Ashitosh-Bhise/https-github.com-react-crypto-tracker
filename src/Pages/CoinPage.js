08e6a37f891f0956c5bcf94bf1560fa216a2e14f
Ashitosh Bhise <132629679+Ashitosh-Bhise@users.noreply.github.com>
2024-02-02 21:54:33 +0530

    Update CoinPage.js

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -22,7 +22,7 @@ const CoinPage = () => {
 
   useEffect(() => {
     fetchCoin();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    
   }, []);
 
   const useStyles = makeStyles((theme) => ({
